770620f457a21f3e5af7a9ac1b023adabafa1286
Dominic Evans <evans.dominic0@gmail.com>
2022-02-21 18:30:37 -0600

    Rudimentary styling and logic

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,1 +1,15 @@
-// Underyling logic for etch-a-sketch program
+const canvas = document.querySelector('#canvas');
+let numPixels = 16;
+
+for (let i = 0; i < canvasSize; i++){
+    let pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixel.textContent = 'test';
+    canvas.appendChild(pixel);
+}
+
+// Determine number of pixels required in the grid
+// Assign each pixel a size so that it creates a neat grid
+function drawCanvas(sideLength){
+
+}
